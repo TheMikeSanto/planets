6f2acd3ec699ec654075609af2418dd5a566edda
Mike <mike.j.santo@gmail.com>
2022-01-21 22:52:23 -0600

    Busted attempt at simple sprite rotation

--- src/sprites/debris.sprite.ts
+++ src/sprites/debris.sprite.ts
@@ -1,7 +1,19 @@
+import * as _ from 'lodash';
 import * as Phaser from 'phaser';
 
+import { randomInRange } from '../utils';
+
+enum SpinDirection {
+  Clockwise,
+  Counterclockwise,
+}
 export class DebrisSprite extends Phaser.GameObjects.Sprite {
   private scaleFactor = Math.random();
+  private shouldRotate = false;
+  private readonly spinDirection = _.sample([
+    SpinDirection.Clockwise,
+    SpinDirection.Counterclockwise
+  ]);
 
   constructor(scene: Phaser.Scene, y: number) {
     super(scene, scene.scale.width + 100, y, 'debris');
@@ -19,7 +31,33 @@ export class DebrisSprite extends Phaser.GameObjects.Sprite {
     return { x: this.x, y: this.y };
   }
 
+  public get size(): number {
+    return this.scaleFactor;
+  }
+
   public update() {
+    if (this.shouldRotate) this.rotate();
     this.setX(this.position.x -(1 - this.scaleFactor));
+  }
+
+  private rotate(): void {
+    const rotationFactor = (1 - this.scaleFactor);
+
+    const angle = (() => {
+      switch (this.spinDirection) {
+        case SpinDirection.Clockwise: 
+          return this.angle < 360
+            ? this.angle + rotationFactor
+            : 0;
+        case SpinDirection.Counterclockwise:
+          return this.angle > 0
+            ? this.angle - rotationFactor
+            : 360;
+      }
+    })();
+    if (this.spinDirection === SpinDirection.Counterclockwise) {
+      console.log('new angle', angle, this.angle, rotationFactor);
+    }
+    this.setAngle(angle);
   }
 }
